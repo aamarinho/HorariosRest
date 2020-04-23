f451d232ce817379abb3a631e9adcb8061117fb9
Andrea <drealvarez97@gmail.com>
2020-04-23 20:17:04 +0200

    Funcionando el generar/registrar en el calendario los eventos de una asignatura dependiendo de su cuatrimestre

--- model/CalendarioMapper.php
+++ model/CalendarioMapper.php
@@ -9,14 +9,40 @@ class CalendarioMapper {
         $this->db = PDOConnection::getInstance();
     }
 
-    public function registrarCalendario(GrupoReducido $grupo) {
+    public function registrar(GrupoReducido $grupo) {
+
+        $stmt = $this->db->prepare("SELECT asignatura.cuatrimestre FROM asignatura INNER JOIN gruporeducido ON gruporeducido.id_asignatura=asignatura.id WHERE gruporeducido.id=?");
+        $stmt->execute(array($grupo->getId()));
+        $resul = $stmt->fetch(PDO::FETCH_ASSOC);
+        $cuatrimestre=$resul['cuatrimestre'];
+
         $stmt = $this->db->prepare("SELECT f_inicio_uno,f_fin_uno,f_inicio_dos,f_fin_dos from configuracion WHERE id=1");
         $stmt->execute();
-        $resul = $stmt->fetchAll(PDO::FETCH_ASSOC); //aqui tengo los valores de inicio y fin de cuatrimestre, para iterar entre ellos
-        //obtener valor de $fecha iterando y obtener valor de $responsable de la tabla usuariogrupo
+        $resul2 = $stmt->fetch(PDO::FETCH_ASSOC); //aqui tengo los valores de inicio y fin de cuatrimestre, para iterar entre ellos
+        if($cuatrimestre==1){
+            $fechainicio=$resul2['f_inicio_uno'];
+            $fechafin=$resul2['f_fin_uno'];
+        } else if($cuatrimestre==2){
+            $fechainicio=$resul2['f_inicio_dos'];
+            $fechafin=$resul2['f_fin_dos'];
+        } else{
+            $fechainicio=$resul2['f_inicio_uno'];
+            $fechafin=$resul2['f_fin_uno'];
+        }
 
-        $stmt = $this->db->prepare("INSERT INTO calendario values (?,?,?,?,?,?)");
-        //$stmt->execute(array($grupo->getId(), $fecha ,$grupo->getHoraInicio(),$grupo->getHoraFin(), $responsable ,$grupo->getAula()));
+        $fechafin = strtotime($fechafin);
+        $fechas=array();
+        for($i = strtotime($grupo->getDia(),strtotime($fechainicio)); $i <= $fechafin; $i = strtotime('+1 week', $i)) {
+            array_push($fechas,date('Y-m-d', $i));
+        }
+        $stmt = $this->db->prepare("SELECT usuariogrupo.email FROM usuariogrupo INNER JOIN gruporeducido ON gruporeducido.id=usuariogrupo.id INNER JOIN usuario ON usuario.email=usuariogrupo.email WHERE usuario.tipo=2 AND usuariogrupo.id=?");
+        $stmt->execute(array($grupo->getId()));
+        $resul = $stmt->fetch(PDO::FETCH_ASSOC);
+        $responsable=$resul['email'];
 
+        foreach($fechas as $fecha){
+            $stmt = $this->db->prepare("INSERT INTO calendario values ('',?,?,?,?,?,?)");
+            $stmt->execute(array($grupo->getId(), $fecha ,$grupo->getHoraInicio(),$grupo->getHoraFin(), $responsable ,$grupo->getAula()));
+        }
     }
 }
---
+++ rest/CalendarioRest.php
@@ -0,0 +1,35 @@
+<?php
+require_once(__DIR__."/../model/GrupoReducidoMapper.php");
+require_once(__DIR__."/../model/UsuarioGrupoMapper.php");
+require_once(__DIR__."/../model/UsuarioGrupo.php");
+require_once(__DIR__ . "/BaseRest.php");
+require_once(__DIR__ . "/../model/GrupoReducido.php");
+require_once(__DIR__ . "/../model/CalendarioMapper.php");
+require_once(__DIR__ . "/../model/Calendario.php");
+require_once(__DIR__."/../core/ValidationException.php");
+
+class CalendarioRest extends BaseRest {
+    private $calendarioMapper;
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->calendarioMapper = new CalendarioMapper();
+    }
+
+    public function registrar() {
+        $data = $_POST['grupos'];
+        $data = json_decode($data, true);
+        foreach ($data as $value) {
+            $gruporeducido = new GrupoReducido($value['id'],$value['id_asignatura'],$value['tipo'],$value['dia'],$value['hora_inicio'],$value['hora_fin'],$value['aula']);
+            $this->calendarioMapper->registrar($gruporeducido);
+        }
+
+        /*header($_SERVER['SERVER_PROTOCOL'] . ' 201 Ok');
+        header('Content-Type: application/json');*/
+    }
+}
+
+$calendario = new CalendarioRest();
+URIDispatcher::getInstance()
+    ->map("POST","/calendario/registrar",array($calendario,"registrar"));
